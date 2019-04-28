0859a7a6517ad47e1081310840842e843fcd746a
Shamnad Shaji <shamnad.shaji@gofore.com>
2019-04-28 22:39:29 +0300

    react native camera in ios

--- android/app/src/main/java/com/patient_poc/MainApplication.java
+++ android/app/src/main/java/com/patient_poc/MainApplication.java
@@ -3,6 +3,7 @@ package com.patient_poc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import org.reactnative.camera.RNCameraPackage;
 import org.wonday.pdf.RCTPdfView;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CameraRollPackage(),
             new RNCameraPackage(),
             new RCTPdfView(),
             new RNFetchBlobPackage(),
--- scenes/main/record.js
+++ scenes/main/record.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, TextInput, View, ScrollView, TouchableOpacity, ActivityIndicator, PermissionsAndroid, CameraRoll } from 'react-native';
-import commonStyles from '../common/styles';
-import Button from '../components/Button';
-import SwitchSelector from "react-native-switch-selector";
-import { RNCamera, FaceDetector } from 'react-native-camera';
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ActivityIndicator
+} from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import { RNCamera } from 'react-native-camera';
+import CameraRoll from "@react-native-community/cameraroll";
+import FlashMessage, { showMessage } from "react-native-flash-message";
 
-export default class Record extends Component {
+export default class CameraScreen extends React.Component {
     static navigationOptions = {
         title: 'Record Consent Video',
         headerTitleStyle: { color: '#F76B8A', textAlign: 'center', alignSelf: 'center' },
@@ -13,212 +19,170 @@ export default class Record extends Component {
             backgroundColor: 'white',
         },
     };
-    constructor(props) {
-        super(props);
-        const { navigation } = this.props;
-        this.state = {
-            recordOptions: {
-                mute: false,
-                maxDuration: 5,
-                quality: RNCamera.Constants.VideoQuality['288p'],
-            },
-            recording: false,
-            processing: false,
-            error: ''
-        }
-    }
-    async componentDidMount() {
-        await this.checkPermission()
+
+    state = {
+        flash: 'off',
+        zoom: 0,
+        depth: 0,
+        type: 'front',
+        whiteBalance: 'auto',
+        ratio: '16:9',
+        recordOptions: {
+            mute: false,
+            maxDuration: 30,
+            quality: RNCamera.Constants.VideoQuality['288p'],
+        },
+        isRecording: false,
+        canDetectFaces: false,
+        canDetectText: false,
+        canDetectBarcode: false,
+        faces: [],
+        textBlocks: [],
+        barcodes: [],
+        processing: false
     };
 
-    async checkPermission() {
-        if (Platform.OS !== 'android') {
-            return Promise.resolve(true);
-        }
+    takeVideo = async function () {
+        if (this.camera) {
+            try {
+                const promise = this.camera.recordAsync(this.state.recordOptions);
 
-        let result;
-        try {
-            result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, { title: 'Microphone Permission', message: 'App needs access to your microphone' });
-            result2 = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, { title: 'Storage Permission', message: 'App needs access to your storage' });
-        } catch (error) {
-            this.setState({ error: error.message });
+                if (promise) {
+                    this.setState({ isRecording: true });
+                    const { uri } = await promise;
+                    this.setState({ isRecording: false, processing: false });
+                    await CameraRoll.saveToCameraRoll(uri, 'video');
+                    showMessage({
+                        message: "Video saved to gallery!",
+                        type: "success",
+                        icon: "success"
+                    });
+                }
+            } catch (e) {
+                console.error(e);
+            }
         }
-        return (result === true || result === PermissionsAndroid.RESULTS.GRANTED && result2 === true || result2 === PermissionsAndroid.RESULTS.GRANTED);
-    }
+    };
 
-    async startRecording() {
-        try {
-            this.setState({ recording: true });
-            // default to mp4 for android as codec is not set
-            const { uri, codec = "mp4" } = await this.camera.recordAsync();
-            this.setState({ recording: false, processing: true });
-            await CameraRoll.saveToCameraRoll(uri, 'video');
-            this.setState({ recording: false, processing: false });
-        } catch (error) {
-            this.setState({ error: error.message });
-        }
+    renderCamera() {
+        const { canDetectFaces, canDetectText, canDetectBarcode } = this.state;
 
-    }
-
-    stopRecording() {
-        try {
-            this.camera.stopRecording();
-        } catch (error) {
-            this.setState({ error: error.message });
-        }
+        return (
+            <RNCamera
+                ref={ref => {
+                    this.camera = ref;
+                }}
+                style={{
+                    flex: 1,
+                    justifyContent: 'space-between',
+                }}
+                type={this.state.type}
+                flashMode={this.state.flash}
+                zoom={this.state.zoom}
+                whiteBalance={this.state.whiteBalance}
+                ratio={this.state.ratio}
+                focusDepth={this.state.depth}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                    title: 'Permission to use audio recording',
+                    message: 'We need your permission to use your audio',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                    console.log(barcodes);
+                }}
+                faceDetectionLandmarks={
+                    RNCamera.Constants.FaceDetection.Landmarks
+                        ? RNCamera.Constants.FaceDetection.Landmarks.all
+                        : undefined
+                }
+                onFacesDetected={canDetectFaces ? this.facesDetected : null}
+                onTextRecognized={canDetectText ? this.textRecognized : null}
+                onGoogleVisionBarcodesDetected={canDetectBarcode ? this.barcodeRecognized : null}
+            >
+                <View style={{ bottom: 0 }}>
+                    <View
+                        style={{
+                            height: 56,
+                            backgroundColor: 'transparent',
+                            flexDirection: 'row',
+                            alignSelf: 'flex-end',
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={[
+                                styles.flipButton,
+                                {
+                                    flex: 0.3,
+                                    alignSelf: 'flex-end',
+                                    backgroundColor: this.state.isRecording ? 'white' : 'darkred',
+                                },
+                            ]}
+                            onPress={this.state.isRecording ? () => { this.setState({ processing: true }) } : this.takeVideo.bind(this)}
+                        >
+                            {this.state.isRecording && !this.state.processing ? (
+                                <Text style={styles.stopText}> STOP </Text>
+                            ) : (this.state.processing ? (<Text style={styles.stopText}> PROCESSING </Text>) :
+                                (<Text style={styles.flipText}> REC </Text>)
+                                )}
+                        </TouchableOpacity>
+                    </View>
+                    <FlashMessage position="top" />
+                </View>
+            </RNCamera>
+        );
     }
 
     render() {
-        const { recording, processing } = this.state;
-
-        let button = (
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={[styles.button]} onPress={this.startRecording.bind(this)} >
-                    <Text style={{ color: 'white' }}>RECORD</Text>
-                </TouchableOpacity>
-            </View >
-        );
-
-        if (recording) {
-            button = (
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={[styles.button]} onPress={this.stopRecording.bind(this)} >
-                        <Text style={{ color: 'white' }}>STOP</Text>
-                    </TouchableOpacity>
-                </View >
-
-            );
-        }
-
-        if (processing) {
-            button = (
-                <View style={styles.capture}>
-                    <ActivityIndicator animating size={18} />
-                </View>
-            );
-        }
-
-        return (
-            <View style={styles.container}>
-                <Text style={{ marginBottom: 20, fontSize: 20, color: '#F76B8A', fontWeight: 'bold' }}>{this.state.error}</Text>
-                <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={styles.preview}
-                    type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
-                    androidCameraPermissionOptions={{
-                        title: 'Permission to use camera',
-                        message: 'We need your permission to use your camera',
-                        buttonPositive: 'Ok',
-                        buttonNegative: 'Cancel',
-                    }}
-                    androidRecordAudioPermissionOptions={{
-                        title: 'Permission to use audio recording',
-                        message: 'We need your permission to use your audio',
-                        buttonPositive: 'Ok',
-                        buttonNegative: 'Cancel',
-                    }}
-                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
-                        console.log(barcodes);
-                    }}
-                />
-                <View
-                    style={{ flex: 0, marginBottom: 40, width: 200, flexDirection: "row", justifyContent: "center" }}
-                >
-                    {button}
-                    <Button navigate='Main'
-                        navigation={this.props.navigation}
-                        position='bottom'
-                        type='plain'
-                        width={120}
-                        text="Cancel" />
-                </View>
-            </View >
-
-        );
+        return <View style={styles.container}>{this.renderCamera()}</View>;
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexGrow: 4,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF',
-    },
-    center: {
-        flexGrow: 4,
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-    },
-    profile: {
-        marginTop: 20,
-        borderColor: '#f76b8a',
-        borderWidth: 1,
-        borderRadius: 100,
-
-    },
-    card: {
-        marginTop: 15,
-        textAlign: 'left',
-        width: 250
-    },
-    cardTop: {
-        textAlign: 'left',
-        width: 300,
-        padding: 10,
-        borderColor: 'lightgrey',
-        borderStyle: 'solid',
-        borderTopWidth: 1,
-    },
-    containerStyle: {
-        flexGrow: 4,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    preview: {
         flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+        paddingTop: 10,
+        backgroundColor: '#000',
     },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 15,
-        paddingHorizontal: 20,
-        alignSelf: 'center',
-        margin: 20,
-    },
-    buttonContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF',
-        marginTop: 15,
-    },
-    button: {
-        fontWeight: Platform.select({
-            ios: '500',
-            android: '200',
-        }),
-        width: 120,
+    flipButton: {
+        flex: 0.3,
+        height: 40,
+        marginHorizontal: 2,
+        marginBottom: 10,
+        marginTop: 10,
+        borderRadius: 8,
+        borderColor: 'white',
         borderWidth: 1,
-        borderRadius: 10,
-        borderColor: 'rgba(157, 163, 180, 0.25)',
-        marginLeft: 5,
-        marginRight: 5,
-        padding: 10,
-        textAlign: 'center',
+        padding: 5,
         alignItems: 'center',
-        backgroundColor: '#F76B8A',
-        color: 'white'
+        justifyContent: 'center',
     },
-    plain: {
-        backgroundColor: '#FFFFFF',
-        color: '#F76B8A',
-        borderColor: '#F76B8A',
-    }
+    flipText: {
+        color: 'white',
+        fontSize: 15,
+    },
+    stopText: {
+        color: 'red',
+        fontSize: 15,
+    },
+    text: {
+        padding: 10,
+        borderWidth: 2,
+        borderRadius: 2,
+        position: 'absolute',
+        borderColor: '#F00',
+        justifyContent: 'center',
+    },
+    textBlock: {
+        color: '#F00',
+        position: 'absolute',
+        textAlign: 'center',
+        backgroundColor: 'transparent',
+    },
 });
